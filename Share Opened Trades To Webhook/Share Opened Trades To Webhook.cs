8af5b7e61448a4639bde7699863bfbd2a40a2cb9
Leonardo <leonardo.ciaccio@gmail.com>
2020-05-20 16:22:35 +0200

    Aggiungere i file di progetto.

---
+++ Share Opened Trades To Webhook/Share Opened Trades To Webhook.cs
@@ -0,0 +1,372 @@
+﻿/*  CTRADER GURU --> Template 1.0.8
+
+    Homepage    : https://ctrader.guru/
+    Telegram    : https://t.me/ctraderguru
+    Twitter     : https://twitter.com/cTraderGURU/
+    Facebook    : https://www.facebook.com/ctrader.guru/
+    YouTube     : https://www.youtube.com/channel/UCKkgbw09Fifj65W5t5lHeCQ
+    GitHub      : https://github.com/ctrader-guru
+
+*/
+
+using System;
+using System.Net;
+using cAlgo.API;
+using System.Windows.Forms;
+using cAlgo.API.Internals;
+
+namespace cAlgo
+{
+
+    // --> Estensioni che rendono il codice più leggibile
+    #region Extensions
+
+    /// <summary>
+    /// Estensione che fornisce metodi aggiuntivi per il simbolo
+    /// </summary>
+    public static class SymbolExtensions
+    {
+
+        /// <summary>
+        /// Converte il numero di pips corrente da digits a double
+        /// </summary>
+        /// <param name="Pips">Il numero di pips nel formato Digits</param>
+        /// <returns></returns>
+        public static double DigitsToPips(this Symbol MySymbol, double Pips)
+        {
+
+            return Math.Round(Pips / MySymbol.PipSize, 2);
+
+        }
+
+        /// <summary>
+        /// Converte il numero di pips corrente da double a digits
+        /// </summary>
+        /// <param name="Pips">Il numero di pips nel formato Double (2)</param>
+        /// <returns></returns>
+        public static double PipsToDigits(this Symbol MySymbol, double Pips)
+        {
+
+            return Math.Round(Pips * MySymbol.PipSize, MySymbol.Digits);
+
+        }
+
+    }
+
+    /// <summary>
+    /// Estensione che fornisce metodi aggiuntivi per le Bars
+    /// </summary>
+    public static class BarsExtensions
+    {
+
+        /// <summary>
+        /// Converte l'indice di una bar partendo dalla data di apertura
+        /// </summary>
+        /// <param name="MyTime">La data e l'ora di apertura della candela</param>
+        /// <returns></returns>
+        public static int GetIndexByDate(this Bars MyBars, DateTime MyTime)
+        {
+
+            for (int i = MyBars.ClosePrices.Count - 1; i >= 0; i--)
+            {
+
+                if (MyTime == MyBars.OpenTimes[i]) return i;
+
+            }
+
+            return -1;
+
+        }
+
+    }
+
+    #endregion
+
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.FullAccess)]
+    public class ShareOpenedTradesToWebhook : Robot
+    {
+
+        #region Enums
+
+        public enum MyColors
+        {
+
+            AliceBlue,
+            AntiqueWhite,
+            Aqua,
+            Aquamarine,
+            Azure,
+            Beige,
+            Bisque,
+            Black,
+            BlanchedAlmond,
+            Blue,
+            BlueViolet,
+            Brown,
+            BurlyWood,
+            CadetBlue,
+            Chartreuse,
+            Chocolate,
+            Coral,
+            CornflowerBlue,
+            Cornsilk,
+            Crimson,
+            Cyan,
+            DarkBlue,
+            DarkCyan,
+            DarkGoldenrod,
+            DarkGray,
+            DarkGreen,
+            DarkKhaki,
+            DarkMagenta,
+            DarkOliveGreen,
+            DarkOrange,
+            DarkOrchid,
+            DarkRed,
+            DarkSalmon,
+            DarkSeaGreen,
+            DarkSlateBlue,
+            DarkSlateGray,
+            DarkTurquoise,
+            DarkViolet,
+            DeepPink,
+            DeepSkyBlue,
+            DimGray,
+            DodgerBlue,
+            Firebrick,
+            FloralWhite,
+            ForestGreen,
+            Fuchsia,
+            Gainsboro,
+            GhostWhite,
+            Gold,
+            Goldenrod,
+            Gray,
+            Green,
+            GreenYellow,
+            Honeydew,
+            HotPink,
+            IndianRed,
+            Indigo,
+            Ivory,
+            Khaki,
+            Lavender,
+            LavenderBlush,
+            LawnGreen,
+            LemonChiffon,
+            LightBlue,
+            LightCoral,
+            LightCyan,
+            LightGoldenrodYellow,
+            LightGray,
+            LightGreen,
+            LightPink,
+            LightSalmon,
+            LightSeaGreen,
+            LightSkyBlue,
+            LightSlateGray,
+            LightSteelBlue,
+            LightYellow,
+            Lime,
+            LimeGreen,
+            Linen,
+            Magenta,
+            Maroon,
+            MediumAquamarine,
+            MediumBlue,
+            MediumOrchid,
+            MediumPurple,
+            MediumSeaGreen,
+            MediumSlateBlue,
+            MediumSpringGreen,
+            MediumTurquoise,
+            MediumVioletRed,
+            MidnightBlue,
+            MintCream,
+            MistyRose,
+            Moccasin,
+            NavajoWhite,
+            Navy,
+            OldLace,
+            Olive,
+            OliveDrab,
+            Orange,
+            OrangeRed,
+            Orchid,
+            PaleGoldenrod,
+            PaleGreen,
+            PaleTurquoise,
+            PaleVioletRed,
+            PapayaWhip,
+            PeachPuff,
+            Peru,
+            Pink,
+            Plum,
+            PowderBlue,
+            Purple,
+            Red,
+            RosyBrown,
+            RoyalBlue,
+            SaddleBrown,
+            Salmon,
+            SandyBrown,
+            SeaGreen,
+            SeaShell,
+            Sienna,
+            Silver,
+            SkyBlue,
+            SlateBlue,
+            SlateGray,
+            Snow,
+            SpringGreen,
+            SteelBlue,
+            Tan,
+            Teal,
+            Thistle,
+            Tomato,
+            Transparent,
+            Turquoise,
+            Violet,
+            Wheat,
+            White,
+            WhiteSmoke,
+            Yellow,
+            YellowGreen
+
+        }
+
+        #endregion
+
+        #region Identity
+
+        /// <summary>
+        /// Nome del prodotto, identificativo, da modificare con il nome della propria creazione
+        /// </summary>
+        public const string NAME = "Share Opened Trades To Webhook";
+
+        /// <summary>
+        /// La versione del prodotto, progressivo, utilie per controllare gli aggiornamenti se viene reso disponibile sul sito ctrader.guru
+        /// </summary>
+        public const string VERSION = "1.0.3";
+
+        #endregion
+
+        #region Params
+
+        /// <summary>
+        /// Identità del prodotto nel contesto di ctrader.guru
+        /// </summary>
+        [Parameter(NAME + " " + VERSION, Group = "Identity", DefaultValue = "https://ctrader.guru/product/trades-to-Webhook/")]
+        public string ProductInfo { get; set; }
+
+        [Parameter("Webhook", Group = "Params", DefaultValue = "https://api.telegram.org/bot[ YOUR TOKEN ]/sendMessage")]
+        public string Webhook { get; set; }
+
+        [Parameter("Message", Group = "Params", DefaultValue = "#{0} opened {1} position at {2} for {3} lots")]
+        public string Message { get; set; }
+
+        [Parameter("POST params", Group = "Params", DefaultValue = "chat_id=[ @CHATID ]&text={0}")]
+        public string PostParams { get; set; }
+
+        [Parameter("Only This Cross ?", Group = "Params", DefaultValue = true)]
+        public bool OonlyThis { get; set; }
+
+        #endregion
+
+        #region Property
+
+        // --> Qui inseriremo variabili e costanti del progetto
+
+        #endregion
+
+        #region cBot Events
+
+        protected override void OnStart()
+        {
+
+            // --> Stampo nei log la versione corrente
+            Print("{0} : {1}", NAME, VERSION);
+
+            // --> Controllo se i valori sono coerenti 
+            Webhook = Webhook.Trim();
+
+            if (Webhook.Length < 1)
+            {
+
+                MessageBox.Show("Wrong 'Webhook', es. 'https://api.telegram.org/bot[ YOUR TOKEN ]/sendMessage'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Stop();
+
+            }
+
+            Message = Message.Trim();
+
+            if (Message.Length < 1)
+            {
+
+                MessageBox.Show("Wrong 'Message', es. '#{0} opened {1} position at {2} for {3} lots'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Stop();
+
+            }
+
+            PostParams = PostParams.Trim();
+
+            if (PostParams.IndexOf("{0}") < 0)
+            {
+
+                MessageBox.Show("Wrong 'POST params', es. 'chat_id=[ @CHATID ]&text={0}'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Stop();
+
+            }
+
+            // --> Registro il callback per condividere le operazioni 
+            Positions.Opened += OnPositionOpened;
+
+        }
+
+        protected override void OnStop()
+        {
+            // --> Rimuovo il callback registrato
+
+            Positions.Opened -= OnPositionOpened;
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+
+        public void OnPositionOpened(PositionOpenedEventArgs args)
+        {
+
+            // --> Condividiamo solo il cross corrente?
+            if (OonlyThis && args.Position.SymbolName != SymbolName)
+                return;
+
+            string messageformat = string.Format(Message, args.Position.SymbolName, args.Position.TradeType, args.Position.EntryPrice, args.Position.Quantity);
+
+            try
+            {
+
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                    string HtmlResult = wc.UploadString(Webhook, string.Format(PostParams, messageformat));
+                }
+
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show(string.Format("{0}\r\nstopping cBots 'Share Opened Trades To Webhook' ...", exc.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Stop();
+
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
